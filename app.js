764429b13e20fb4e492dcb41bfa06a3af21795a3
akhilchoudhary2k <akhil.choudhary2000@gmail.com>
2020-04-25 14:21:50 +0530

    working passport session (only registration)

--- app.js
+++ app.js
@@ -1,38 +1,52 @@
     //jshint esversion:6
 
     const express = require('express');
+    const ejs = require('ejs');
     const bodyParser = require('body-parser');
     const mongoose = require('mongoose');
-    const md5 = require('md5');
-
+    const session = require('express-session');
+    const passport = require("passport");
+    const passportLocalMongoose = require('passport-local-mongoose');
 
     //setup server
     const app = express();
+
     app.set("view engine" , "ejs");
     app.use(bodyParser.urlencoded({extended:true}));
     app.use(express.static("public"));   // to correctly send the images and css files
 
+    app.use(session({
+        secret: "ourLittleSecretKey.",
+        resave: false,
+        saveUninitialized: false
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
     // making a local database named mindmatchDB
     mongoose.connect( "mongodb://localhost:27017/MindMatchDB", { useNewUrlParser: true, useUnifiedTopology: true } );
+    // mongoose.set("useCreateIndex", true);
 
     // making a schema for the user info
-    const userSchema = new mongoose.Schema({
-        name : String,
-        email : String,
+    const userSchema =  mongoose.Schema({
+        username : String,
+        // email : String,
         password : String
     });
+    userSchema.plugin(passportLocalMongoose);
 
     // making a model of this userSchema which will represent a collection in MindMatchDB database
     const User = new mongoose.model("User", userSchema); // this will be cleverly converted to plural
 
-
+    passport.use(User.createStrategy());
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
 
 
     //get request to server
     app.get("/", function(req,res){
         //sending information from server side
         res.render("index", {});
-        // res.sendFile(__dirname + "/index.html");
     });
 
 
@@ -51,37 +65,31 @@
         res.render('about',{});
     });
     app.get("/contact", function(req,res){
-        console.log(req.body);
         res.render('contact',{});
     });
 
     app.post("/login",function(req,res){
-        // console.log(req.body);
-        const email = req.body.email;
-        const password = md5(req.body.password);
-        User.findOne({email:email}, function(err, foundUser){
-            if(err){
-                console.log(err);
-            } else{
-                if(foundUser){
-                    if(foundUser.password === password){
-                        res.render('UserHome', {user,foundUser});
-                    }
-                }
 
-            }
-        });
+    });
+    app.get("/UserHome",function(req,res){
+        if(req.isAuthenticated()){
+            res.render('UserHome',{});
+        } else{
+            res.redirect('/login');
+        }
     });
     app.post("/register",function(req,res){
         // console.log(req.body);
-        const newUser = new User({
-            name : req.body.name,
-            email: req.body.email,
-            password: md5(req.body.password1)
+        User.register( {username:req.body.username} , req.body.password , function(err,user){
+            if(err){
+                console.log(err);
+                res.redirect("/register");
+            } else{
+                passport.authenticate("local")(req,res,function(){
+                    res.redirect("/UserHome");
+                });
+            }
         });
-        newUser.save();
-        res.locals.title = req.body.name+" 's HomePage";
-        res.render('UserHome', {user: newUser});
     });
     app.post("/adminlogin",function(req,res){
 
